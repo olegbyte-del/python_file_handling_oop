56f913f60b9365ace3e8ef0839fbd45015e787cd
olegbyte-del <angeloferrer5436@gmail.com>
2026-04-22 17:17:16 +0800

    Add OOP RandomNumberGenerator class and test implementation
    - Implemented RandomNumberGenerator class for generating random integers
    - Added method to generate numebrs within defined range and count
    - Created test run in main.py to validate class functionality
    - Verified random number generation output before file processing integration

--- p1_even_odd_file_seperator_oop/number_generator_seperator.py
+++ p1_even_odd_file_seperator_oop/number_generator_seperator.py
@@ -1,1 +1,14 @@
 # Number Generator & Seperator Program
+
+import random 
+
+class RandomNumberGenerator:
+    
+    def __init__(self, count, start, end):
+        self.count = count
+        self.start = start 
+        self.end = end 
+    
+    def randint_gen(self):
+        return [random.randint(self.start, self.end) for _ in range(self.count)]
+    
