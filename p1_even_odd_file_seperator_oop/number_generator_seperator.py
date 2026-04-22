7f86e64e198a5a694fdcb4d798a89941040589a9
olegbyte-del <angeloferrer5436@gmail.com>
2026-04-23 00:29:49 +0800

    Add EvenNumberProcessor class with file reading and initial testing
    - Created EvenNumberProcessor class for handling even number processing
    - Implemented __init__ method with input file parameter
    - Initialized self.numbers and self.even_numbers for data storage
    - Added read_file method to read numbers from number.txt
    - Converted string values to integers and stored in self.numbers
    - Tested class functionality in main

--- p1_even_odd_file_seperator_oop/number_generator_seperator.py
+++ p1_even_odd_file_seperator_oop/number_generator_seperator.py
@@ -12,4 +12,18 @@ class RandomNumberGenerator:
     
     def randint_gen(self):
         return [random.randint(self.start, self.end) for _ in range(self.count)]
+
+class EvenNumberProcessor:
+    """Identifies if a number is an even number from the list of numbers"""
+
+    def __init__(self, input_file):
+        self.input_file = input_file
+        self.numbers = []
+        self.even_numbers = []
+        
+    def read_file(self):
+        with open(self.input_file, "r") as file: 
+            for line in file:
+                num = int(line.strip())
+                self.numbers.append(num) 
     
