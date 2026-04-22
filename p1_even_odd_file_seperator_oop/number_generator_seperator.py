62fea812c9fea7b175b122065ec7f9bf94fd5026
olegbyte-del <angeloferrer5436@gmail.com>
2026-04-22 17:26:21 +0800

    Add docstring to RandomNumberGenerator class
    - Added class-level docstring in RandomNumberGenerator class

--- p1_even_odd_file_seperator_oop/number_generator_seperator.py
+++ p1_even_odd_file_seperator_oop/number_generator_seperator.py
@@ -3,7 +3,8 @@
 import random 
 
 class RandomNumberGenerator:
-    
+    """Generates random integers based on count, start, and end range."""
+        
     def __init__(self, count, start, end):
         self.count = count
         self.start = start 
