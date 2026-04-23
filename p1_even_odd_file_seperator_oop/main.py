90a37b72fa0bb00a6c1ee87f21c7ec565d05c677
olegbyte-del <angeloferrer5436@gmail.com>
2026-04-23 14:36:48 +0800

    feat: improve validation and add number list display in generator module
    - Add isdigit to prevent invalid menu selection
    - Convert user_input to INT
    - Initialize number_list inside option 1 to store generated numbers
    - Updated file reading logic to load numbers from number.text into a list
    - Displayed Generated numbers for better visualization
    - Added exit option to break the look or program
    - Utilize try-except for error handling

--- p1_even_odd_file_seperator_oop/main.py
+++ p1_even_odd_file_seperator_oop/main.py
@@ -16,3 +16,37 @@ while True:
         print("="*50)
 
         user_input = input("Please enter your choice: ")
+
+        if not user_input.isdigit():
+            print("Invalid input. Try again.")
+            continue
+
+        user_input = int(user_input)
+
+        if user_input == 1:
+            number_list = []
+            start_number = int(input("Starting point: "))
+            end_number = int(input("End point: "))
+
+            generator = RandomNumberGenerator(20, start_number, end_number)
+            generator.randint_gen()
+
+            with open("numbers.txt", "r") as file:
+                for line in file:
+                    number_list.append(int(line.strip()))
+                    
+            print("="*50)
+            print(number_list)
+
+            input("\nPress Enter to continue...")
+
+        elif user_input == 0:
+            print("Exiting the program...")
+            break
+
+        else:
+            print("Invalid choice try again...")
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        input("Press Enter to continue...")
