f048e15f10a4baac83363e8b79236ace1b6dc9c1
olegbyte-del <angeloferrer5436@gmail.com>
2026-04-25 00:58:03 +0800

    feat: Faculty Panel interface in the main program
    - Utilize loop for the menu
    - initialize student generator to create random student records
    - display generated student list from student_list.txt
    - Add error handling

--- p2_student_gwa_analysis_oop/main.py
+++ p2_student_gwa_analysis_oop/main.py
@@ -2,6 +2,30 @@
 
 import student_manager as sm 
 
-create_file = sm.StudentAnalysis()
+analysis = sm.StudentAnalysis()
 
-create_file.highest()
+while True:
+    try:
+        print("="*50, "Faculty Panel".center(50), "="*50, sep="\n")
+        print("[1] Create Random student record",
+            "[2] Analyze Highest Student", 
+            "[3] Exit", "="*50, sep = "\n")
+        
+        user_input = int(input())
+        
+        if user_input == 1:
+            sm.StudentGenerator.generate_student_file()
+            
+            print("="*50)
+            with open("students_list.txt", "r") as file:
+                for line in file:
+                    print(line.strip())
+            print("="*50)
+            
+            input("Press enter to go back.")
+        
+        
+    except Exception as e:
+        print(f"Error Occured! {e}")
+        
+
