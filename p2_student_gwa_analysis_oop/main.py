415017722f0b12584aa620395175297fe89d3af9
olegbyte-del <angeloferrer5436@gmail.com>
2026-04-24 23:33:20 +0800

    refactor: covert StudentGenerator class into a static method for file generation
    - remove instances __init__ and self
    - converted method to class method for easy file creation
    - change students into a list and move first_name and last_names inside the static method generate_student_file()

--- p2_student_gwa_analysis_oop/main.py
+++ p2_student_gwa_analysis_oop/main.py
@@ -1,1 +1,5 @@
 # Main program
+
+import student_manager as sm 
+
+create_file = sm.StudentGenerator.generate_student_file()
--- p2_student_gwa_analysis_oop/student_manager.py
+++ p2_student_gwa_analysis_oop/student_manager.py
@@ -4,20 +4,27 @@ import random
 
 class StudentGenerator:
     
-    def __init__(self, filename, count = 20):
-    
-        self.first_names = [
+    def generate_student_file(filename = "students_list.txt"):
+        students = []
+        
+        first_names = [
             "Juan", "Maria", "Pedro", "Ana", "Mark",
             "Carla", "John", "Elena", "Jose", "Sofia",
             "Miguel", "Paula", "Daniel", "Angela", "Kevin",
             "Nicole", "Ryan", "Gabriela", "Patrick", "Isabella"
         ]
 
-        self.last_names = [
+        last_names = [
             "Cruz", "Santos", "Reyes", "Lopez", "Garcia",
             "Gomez", "Ramos", "Martinez", "Fernandez", "Rivera"
         ]
     
-
-            
+        for _ in range(20): 
+            name = f"{random.choice(first_names)} {random.choice(last_names)}"
+            gwa = round(random.uniform(1.00, 3.00), 2)
+            students.append((name, gwa))
+        
+        with open(filename, "w") as file:
+            for name, gwa in students:
+                file.write(f"{name}, {gwa}\n")
             
