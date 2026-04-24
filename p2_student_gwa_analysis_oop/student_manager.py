9f9bc5ef13cb0107acee18e2c9862c442bb6a148
olegbyte-del <angeloferrer5436@gmail.com>
2026-04-24 23:17:45 +0800

    feat: initialize StudentGenerator file and main program
    - Created StudentGenerator class for generating random dictionary of students
    - included random first_name and last_name for name generation
    - __init__ count to fix = 20 studnets

---
+++ p2_student_gwa_analysis_oop/student_manager.py
@@ -0,0 +1,23 @@
+# Student Manager
+
+import random
+
+class StudentGenerator:
+    
+    def __init__(self, filename, count = 20):
+    
+        self.first_names = [
+            "Juan", "Maria", "Pedro", "Ana", "Mark",
+            "Carla", "John", "Elena", "Jose", "Sofia",
+            "Miguel", "Paula", "Daniel", "Angela", "Kevin",
+            "Nicole", "Ryan", "Gabriela", "Patrick", "Isabella"
+        ]
+
+        self.last_names = [
+            "Cruz", "Santos", "Reyes", "Lopez", "Garcia",
+            "Gomez", "Ramos", "Martinez", "Fernandez", "Rivera"
+        ]
+    
+
+            
+            
