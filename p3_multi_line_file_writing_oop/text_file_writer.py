336927eecd085c09d65066a5173afdef22e4c019
olegbyte-del <angeloferrer5436@gmail.com>
2026-04-25 01:34:02 +0800

    feat: add FileManager class for multi-line text file writing
    - dyanmic filename input for user customization
    - automatic .txt extension handling
    - user option to continue writing lines
    - exception handling included for input errors

--- p3_multi_line_file_writing_oop/text_file_writer.py
+++ p3_multi_line_file_writing_oop/text_file_writer.py
@@ -1,2 +1,28 @@
 # File and line generator
 
+class FileManager:
+
+    filename = input("What will be the name of the file?: ") + ".txt"
+    
+    def create_file():
+        with open(FileManager.filename, "w") as file:
+            def enter_text():
+                while True:
+                    try:
+                        user_input = input("Enter line: ")
+                        file.write(user_input)
+                        
+                        additional_input = input("Are there more lines y/n?: ").lower()
+                        
+                        if additional_input == "y":
+                            enter_text()
+                        
+                        elif additional_input == "n": 
+                            print(f)
+                    
+                    except Exception as e:
+                        print(f"Error Occured! {e}")
+                
+                
+            
+            
