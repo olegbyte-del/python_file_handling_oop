46ca2cabe5319058f10c38811d9f1a4f15816b57
olegbyte-del <angeloferrer5436@gmail.com>
2026-04-25 11:57:36 +0800

    feat: improve FileManager with integer file generation and safedeletion of file
    - fix intger file writing by converting numbers to string before writing
    - improved create_file method to always generate 20 random integers
    - confirmation user input for removing a file

--- p4_number_transformation_files_oop/integer_processor.py
+++ p4_number_transformation_files_oop/integer_processor.py
@@ -1,12 +1,14 @@
 # Integer file processor
 
 import random
+import os
 
 class FileManager:
     
     filename = "integers.txt"
     
-    def create_file(self, count = 20):
+    def create_file():
+        count = 20
         random_numbers = []
         
         start = int(input("Number will start: "))
@@ -16,4 +18,22 @@ class FileManager:
                 random_numbers.append(random.randint(start, end))
         
         with open(FileManager.filename, "w") as file: 
+            for num in random_numbers:
+                file.write(str(num) + "\n")
             
+            print(f"{FileManager.filename} successfully created!")
+            
+    def remove_file():
+        print(f"Are you sure you want to delete '{FileManager.filename}'? y/n ")
+        confirmation = input("").lower()
+        
+        if confirmation == "y":
+            if os.path.exists(FileManager.filename):
+                os.remove(FileManager.filename)
+                print(f"Successfully deleted '{FileManager.filename}'.")
+        
+        elif confirmation == "n":
+            return
+        
+        else:
+            print("Invalid input!")
--- p4_number_transformation_files_oop/main.py
+++ p4_number_transformation_files_oop/main.py
@@ -1,1 +1,5 @@
 # Main Program
+
+import integer_processor as ip 
+
+ip.FileManager.create_file()
