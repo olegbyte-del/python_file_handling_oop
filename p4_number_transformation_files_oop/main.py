f3cdc4505be5a65c57e07e7d3a170a213208fae4
olegbyte-del <angeloferrer5436@gmail.com>
2026-04-25 14:04:34 +0800

    feat: Add Number Analysis submenu to main program
    - Implemented Number Analysis menu under main program option 2
    - added option for even analysis, odd analysis, and square/cube analysis
    - integrated NumberAnalysis Class method into menu system
    - Improved program navigation with submenu loop and back option

--- p4_number_transformation_files_oop/main.py
+++ p4_number_transformation_files_oop/main.py
@@ -1,43 +1,87 @@
 # Main Program
 
-import integer_processor as ip 
+# Main Program
+
+import integer_processor as ip
 
 analysis = ip.NumberAnalysis()
-read = ip.FileManager()
 
 while True:
     try:
         print("="*50, "Number Analysis".center(50), "="*50, sep="\n")
-        print("[1] File Manager", 
+        print("[1] File Manager",
             "[2] Number Analysis",
-            "[0] Exit", "="*50, sep="\n")
+            "[0] Exit",
+            "="*50, sep="\n")
+
+        user_input = input("Enter choice: ")
         
-        user_input = int(input())
-        
-        if user_input == 1:
-            print("="*50, "File Manager".center(50), "="*50, sep="\n")
-            print("[1] Create Random 20 Integer File", 
-            "[2] Remove Integer File",
-            "[3] Remove All Files",
-            "[0] Exit", "="*50, sep="\n")
-            
-            user_choice = int(input())
-            
-            if user_choice == 1: 
-                ip.FileManager.create_file()
-            
-            elif user_choice == 2:
-                ip.FileManager.remove_file()
+        if user_input == "1":
+            while True:
+                print("="*50, "File Manager".center(50), "="*50, sep="\n")
+                print("[1] Create Random 20 Integer File",
+                    "[2] Remove Integer File",
+                    "[3] Remove All Files",
+                    "[0] Back",
+                    "="*50, sep="\n")
+
+                user_choice = input("Enter choice: ")
+
+                if user_choice == "1":
+                    ip.FileManager.create_file()
+                    input("Press enter to continue...")
+
+                elif user_choice == "2":
+                    ip.FileManager.remove_file()
+                    input("Press enter to continue...")
+
+                elif user_choice == "3":
+                    ip.FileManager.remove_all_files()
+                    input("Press enter to continue...")
+
+                elif user_choice == "0":
+                    print("Returning to main menu...")
+                    break
+
+                else:
+                    print("Invalid input!")
+
+        elif user_input == "2":
+            while True:
+                print("="*50, "Number Analysis".center(50), "="*50, sep="\n")
+                print("[1] Even Analysis",
+                    "[2] Odd Analysis",
+                    "[3] Square(Even) Cube(Odd) Analysis",
+                    "[0] Back",
+                    "="*50, sep="\n")
+
+                user_choice = input("Enter choice: ")
+
+                if user_choice == "1":
+                    analysis.even_analysis()
+                    input("Press enter to continue...")
+
+                elif user_choice == "2":
+                    analysis.odd_analysis()
+                    input("Press enter to continue...")
+
+                elif user_choice == "3":
+                    analysis.square_cube_analysis()
+                    input("Press enter to continue...")
+
+                elif user_choice == "0":
+                    print("Returning to main menu...")
+                    break
+
+                else:
+                    print("Invalid input!")
                 
-            elif user_choice == 3:
-                ip.FileManager.remove_all_files()
-            
-            elif user_choice == 0:
-                
-                
-            else:
-                print("Invalid input!")
-        
+        elif user_input == "0":
+            print("Exiting the Program...")
+            break
+
+        else:
+            print("Invalid input!")
+
     except Exception as e:
         print(f"Error Occured! {e}")
-
