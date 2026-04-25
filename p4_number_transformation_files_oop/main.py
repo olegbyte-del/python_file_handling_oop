ff29c86f02d221206dc1283ca8cf6a3f5ab14809
olegbyte-del <angeloferrer5436@gmail.com>
2026-04-25 13:21:29 +0800

    feat: add main menu structure for number analysis program
    - imported integer_processory moduel as ip
    - initialize FileManager and NumberAnalysis objects
    - created main program loop for user interaction
    - basic exception handling

--- p4_number_transformation_files_oop/main.py
+++ p4_number_transformation_files_oop/main.py
@@ -3,5 +3,15 @@
 import integer_processor as ip 
 
 analysis = ip.NumberAnalysis()
+read = ip.FileManager()
 
-analysis.square_cube_analysis()
+while True:
+    try:
+        print("="*50, "Number Analysis".center(50), "="*50, sep="\n")
+        print("[1] File Manager", 
+            "[2] Number Analysis",
+            "[0] Exit", "="*50, sep="\n")
+        
+    except Exception as e:
+        print(f"Error Occured! {e}")
+
