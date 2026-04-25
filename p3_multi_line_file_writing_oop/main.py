476794d56bf3d8fa960892bd93e917142313d399
olegbyte-del <angeloferrer5436@gmail.com>
2026-04-25 10:28:20 +0800

    feat: add main menu interface for text writer program
    - created loop-based menu for user interaction
    - added option to create file using FileManager class
    - included exit option for program termination
    - input validation for menu selection using conditional if-else
    - Integrated exception handling for runtime errors

--- p3_multi_line_file_writing_oop/main.py
+++ p3_multi_line_file_writing_oop/main.py
@@ -2,4 +2,23 @@
 
 import text_file_writer as tf
 
-tf.FileManager.create_file()
+while True:
+    try:
+        print("="*50, "Text Writer Program".center(50), "="*50, sep="\n")
+        print("[1] Create file",
+            "[0] Exit", "="*50, sep="\n")
+        
+        user_input = int(input())
+        
+        if user_input == 1:
+            tf.FileManager.create_file()
+            print(f"Press enter to continue...")
+        
+        elif user_input == 0:
+            print("Exiting the program...")
+        
+        else:
+            print("Invalid input! Try again.")
+    
+    except Exception as e:
+        print(f"Error Occured! {e}")
