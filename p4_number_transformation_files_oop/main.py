9e9f098032efbf162095071d4b20b0cc21a9803c
olegbyte-del <angeloferrer5436@gmail.com>
2026-04-25 13:32:43 +0800

    feat: add file manager submenu with file operations
    - Implemented File Manager menu under main program option1
    - added option to create, delete single file, and delete all files
    - connected menu action to FileManager methods in integer_processor module
    - added input handling for user selection

--- p4_number_transformation_files_oop/main.py
+++ p4_number_transformation_files_oop/main.py
@@ -12,6 +12,32 @@ while True:
             "[2] Number Analysis",
             "[0] Exit", "="*50, sep="\n")
         
+        user_input = int(input())
+        
+        if user_input == 1:
+            print("="*50, "File Manager".center(50), "="*50, sep="\n")
+            print("[1] Create Random 20 Integer File", 
+            "[2] Remove Integer File",
+            "[3] Remove All Files",
+            "[0] Exit", "="*50, sep="\n")
+            
+            user_choice = int(input())
+            
+            if user_choice == 1: 
+                ip.FileManager.create_file()
+            
+            elif user_choice == 2:
+                ip.FileManager.remove_file()
+                
+            elif user_choice == 3:
+                ip.FileManager.remove_all_files()
+            
+            elif user_choice == 0:
+                
+                
+            else:
+                print("Invalid input!")
+        
     except Exception as e:
         print(f"Error Occured! {e}")
 
